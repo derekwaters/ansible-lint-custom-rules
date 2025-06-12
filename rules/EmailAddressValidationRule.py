2dc28e37f813f6cdd5648a05867db5f6463a3527
derek <derek@frisbeeworld.com>
2025-06-12 12:28:08 +1000

    Implementation of email validation (with customisation)

--- rules/EmailAddressValidationRule.py
+++ rules/EmailAddressValidationRule.py
@@ -13,38 +13,67 @@ import re
 if TYPE_CHECKING:
     from ansiblelint.file_utils import Lintable
 
+C_WHITELIST_EMAIL_DOMAINS: str = "whitelistemaildomains"
+
+ID: str = "email_address_validation"
+DESC: str = """Rule to check if any emails sent from modules are to defined domains.
+
+- Options
+
+  - ``whitelistemaildomains`` lists the allowed email domains
+
+- Configuration
+
+  .. code-block:: yaml
+  
+  rules:
+    email_address_validation:
+      whitelistemaildomains:
+        - customer.com.au
+        - customer.com
+        - cust.com.au
+"""
+
+WHITELIST_EMAIL_DOMAINS: typing.FrozenSet[str] = frozenset("""
+customer.com.au
+customer.com
+cust.com.au
+""".split())
 
 class EmailAddressValidationRule(AnsibleLintRule):
-    """Email addresses provided to the If any task uses:
-- The `community.general.mail` module
-- A `uri` module or raw `telnet` to port 25 (SMTP)
+    """Email addresses provided to must use valid domains"""
 
-Then the recipient email address must be in the `@customer.com.au` domain. This rule ensures email notifications do not leak externally and requires regex validation on `to:` or `rcpt_to:` fields.
-
-    ."""
-
-    id = "email-address-validation"
-    description = (
-        "Sending email should only use valid email addresses"
-    )
+    id : str = ID
+    shortdesc: str = "Email Address Validation"
+    description: str = DESC
     severity = "VERY_HIGH"
     tags = ["email", "customer"]
     version_changed = "5.0.11"
 
-    allowed_email_domains = [
-        "customer.com.au",
-        "cust.com.au"
+    fields_to_check = [
+        "bcc",
+        "cc",
+        "to"
     ]
+
+    def allowed_email_domains(self):
+        allowed = self.get_config(C_WHITELIST_EMAIL_DOMAINS)
+        if allowed:
+            return frozenset(allowed)
+        
+        return WHITELIST_EMAIL_DOMAINS
+    
 
     def allowedemail(
         self,
         checkemail
     ) -> bool:
-        matches = re.search("^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$". checkemail)
-        if matches is None:
+        matches = re.search("^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$", checkemail)
+        print(matches)
+        if matches is None or matches.group(1) is None:
             return False
-        domain = matches.group()
-        if domain in self.allowed_email_domains:
+        domain = matches.group(1)
+        if domain in self.allowed_email_domains():
             return True
         return False
 
@@ -53,17 +82,19 @@ Then the recipient email address must be in the `@customer.com.au` domain. This 
         task: Task,
         file: Lintable | None = None,
     ) -> bool | str:
-        if task["action"]["__ansible_module__"] == "mail":
+        if task["action"]["__ansible_module__"] == "community.general.mail" or task["action"]["__ansible_module__"] == "mail":
             checkemails = []
             invalidemails = []
-            checkemails.append(task["action"].get("bcc"))
-            checkemails.append(task["action"].get("cc"))
-            checkemails.append(task["action"].get("to"))
+            for field_to_check in self.fields_to_check:
+                value = task["action"].get(field_to_check)
+                if value is not None:
+                    checkemails.append(value)
+                    
             for checkemail in checkemails:
                 if not self.allowedemail(checkemail):
-                    invalidemails.push(checkemail)
+                    invalidemails.append(checkemail)
 
             if len(invalidemails) > 0:
-                return False
-        return True
+                return "The following emails reference invalid domains: " + ", ".join(invalidemails)
+
     
